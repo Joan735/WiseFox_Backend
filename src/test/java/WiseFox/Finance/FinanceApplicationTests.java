c7691546e07d2bd81d3661c1cde6aafd3b1c61c0
Joan735 <joanye777@gmail.com>
2026-02-18 16:19:53 +0100

    First commit

---
+++ src/test/java/WiseFox/Finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package WiseFox.Finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
