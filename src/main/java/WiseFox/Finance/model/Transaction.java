f2fa9c02146cfdbc6aca1033f1c57474303b1b0e
Joan735 <joanye777@gmail.com>
2026-03-11 16:12:46 +0100

    Change amount type from Double to BigDecimal

--- src/main/java/WiseFox/Finance/model/Transaction.java
+++ src/main/java/WiseFox/Finance/model/Transaction.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.Data;
 
 @Entity
@@ -22,8 +23,8 @@ public class Transaction {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
-	private Double amount;
+	@Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal amount;
 
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
@@ -58,11 +59,11 @@ public class Transaction {
 		this.id = id;
 	}
 
-	public Double getAmount() {
+	public BigDecimal getAmount() {
 		return amount;
 	}
 
-	public void setAmount(Double amount) {
+	public void setAmount(BigDecimal amount) {
 		this.amount = amount;
 	}
 
